4b73b3cfb9347d843f6a058b76d98e791871daa1
Michael Fuget <michaelfuget@gmail.com>
2021-02-15 02:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class Deciduous extends Tree {
   }
   static definition()
   {
-    return "${super.definition()} Deciduous trees shed their leaves annually."
+    return super.definition() + " Deciduous trees shed their leaves annually."
   }
 }
 class Evergreen extends Tree {
